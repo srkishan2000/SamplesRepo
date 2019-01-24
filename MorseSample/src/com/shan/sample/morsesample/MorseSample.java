3d41a03ff69a41df593b4bc00fdbc29eab050c2c
Shan <srikishan2000@gmail.com>
2019-01-24 21:20:32 +0200

    minor changes

--- MorseSample/src/com/shan/sample/morsesample/MorseSample.java
+++ MorseSample/src/com/shan/sample/morsesample/MorseSample.java
@@ -17,7 +17,6 @@ public class MorseSample {
             ".----","..---","...--","....-",".....","-....","--...","---..","----.","-----"
             };
 	
-	//,"\\.","\\,","\\?","\\/","\\@"
 	private static final String[] ALPHABET = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",".","\\,","\\?","\\/","\\@","1","2","3","4","5","6","7","8","9","0"};
 	
 	public static void main(String[] args) throws Exception
